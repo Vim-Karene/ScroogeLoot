8d839dd35c4c3e3ef09012139c94493c15981004
Kärene <143636066+Vim-Karene@users.noreply.github.com>
2025-05-24 20:34:54 +0200

    Add files via upload

---
+++ Locale/esES.lua
@@ -0,0 +1,13 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("ScroogeLoot", "esES")
+
+L["ScroogeLoot Token Sheet"] = "Hoja de puntos de ScroogeLoot"
+L["Send"] = "Enviar"
+L["Import XML"] = "Importar XML"
+L["Paste XML:"] = "Pegar XML:"
+L["Send data to which player?"] = "¿Enviar datos a qué jugador?"
+L["Imported player data from %s"] = "Datos de jugador importados de %s"
+L["Failed to export player data."] = "Error al exportar los datos del jugador."
+L["Player data exported to %s"] = "Datos del jugador exportados a %s"
+L["Raid Started"] = "Redada iniciada"
+L["Raid Concluded"] = "Redada concluida"
+L["%s (%d/%d, %d%% attendance)"] = "%s (%d/%d, %d%% asistencia)"
